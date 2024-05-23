a8b1c28475a95a075730c4afc9e36fd308c83bf8
neuralninja <ninjaneural@gmail.com>
2024-05-23 20:47:03 +0900

    no-gradio-queue 추가

---
+++ javascript/temporalnet_util.js
@@ -0,0 +1,8 @@
+
+
+onUiLoaded((e)=>{
+    // on load update
+    setTimeout(()=>{
+        document.querySelector('#temporalnet_util_update_button').click();
+    }, 1000);
+});
--- scripts/ui.py
+++ scripts/ui.py
@@ -2,7 +2,7 @@ import os
 import gradio as gr
 import threading
 import time
-from modules import script_callbacks
+from modules import shared, script_callbacks
 # from modules.ui_components import FormRow, FormGroup, ToolButton, FormHTML
 # from modules.ui import  create_sampler_and_steps_selection
 # from modules import ui_extra_networks, devices, shared, scripts, script_callbacks, sd_hijack_unet, sd_hijack_utils
@@ -14,8 +14,11 @@ from controlnet_ext import (
     get_cn_models,
 )
 
+def is_queue_enabled():
+    # `queue_enabled` 속성이 존재하는지 확인하고, 존재하면 그 값을 반환
+    return getattr(shared.demo, 'enable_queue', False)
+
 # Global variable to control the cancellation
-cancel_process = threading.Event()
 share_value = {"cancel":False}
 
 def extract_video(video, fps, output_path):
@@ -37,6 +40,12 @@ def ui_buttons(status, complete = False):
         return status, "complete", gr.Button.update(visible=True), gr.Button.update(value="Cancel", visible=False)
     else:
         return status, "", gr.Button.update(visible=False), gr.Button.update(value="Cancel", visible=True)
+
+def process_video_noqueue(input_video_mode,upload_video,video_path,input_folder,force_fps,resize_width,resize_height,target_fps,seed,sampler,sampler_step,cfg_scale,temporalnet_ver,temporalnet_model,temporalnet_weight,prompt,neg_prompt,denoise,output_images_folder,overwrite_output_images,output_video):
+    for status, complete, btn1, btn2 in process_video(input_video_mode,upload_video,video_path,input_folder,force_fps,resize_width,resize_height,target_fps,seed,sampler,sampler_step,cfg_scale,temporalnet_ver,temporalnet_model,temporalnet_weight,prompt,neg_prompt,denoise,output_images_folder,overwrite_output_images,output_video):
+        print(status)
+    
+    return ui_buttons(status, True)
 
 def process_video(input_video_mode,upload_video,video_path,input_folder,force_fps,resize_width,resize_height,target_fps,seed,sampler,sampler_step,cfg_scale,temporalnet_ver,temporalnet_model,temporalnet_weight,prompt,neg_prompt,denoise,output_images_folder,overwrite_output_images,output_video):
     global share_value
@@ -252,14 +261,24 @@ def create_temporal_net_util(sampler_names):
             with gr.Row():
                 output_images_folder = gr.Textbox(label="Output Images Folder (TemporalNet-Util)", placeholder="Folder to output converted frame images")
                 overwrite_output_images = gr.Checkbox(label="Overwrite Output Images", placeholder="Overwrite output converted frame images")
+
             with gr.Row():
                 target_fps = gr.Number(value=30, precision=1, label="Output Video FPS", interactive=True)
             with gr.Row():
                 output_video = gr.Textbox(label="Output Video File (If left blank, it will be automatically saved)", placeholder="Output Video File Path (eg. d:/work/output/smart.mp4)")
 
-            with gr.Row():
-                start_button = gr.Button("Run", variant='primary', elem_id="temporalnet_util_start_button") 
-                cancel_button = gr.Button("Cancel", visible=False, elem_id="temporalnet_util_cancel_button")
+            with gr.Row() as queue_evn_buttons:
+                start_button = gr.Button("Run", variant='primary') 
+                cancel_button = gr.Button("Cancel", visible=False)
+
+            with gr.Column() as noqueue_evn_buttons:
+                with gr.Row():
+                    start_noqueue_button = gr.Button("Run (No Queue)", variant='primary', elem_id="temporalnet_util_start_button") 
+                    cancel_noqueue_button = gr.Button("Cancel", elem_id="temporalnet_util_cancel_button")
+                gr.Markdown('queue를 지원하지 않습니다. 진행상황이 Console 출력으로 표시됩니다. (--no-gradio-queue 옵션이 있다면 빼주세요)')
+
+            with gr.Row(visible=False):
+                update_button = gr.Button("Update", elem_id="temporalnet_util_update_button") 
 
             with gr.Row():
                 status = gr.Textbox(
@@ -279,11 +298,34 @@ def create_temporal_net_util(sampler_names):
         share_value["cancel"] = True
         # time.sleep(3)
         return gr.Button.update(value="Canceling")
+    
+    def handle_cancel_noqueue():
+        global share_value
+        print('share_value',share_value)
+        share_value["cancel"] = True
+        # time.sleep(3)
+        return gr.Button.update(value="Cancel")
+    
+    def update_state():
+        queue_enabled = is_queue_enabled()
+        print('queue_enabled', queue_enabled)
+        if queue_enabled:
+            return gr.Row().update(visible=True), gr.Row().update(visible=False)
+        else:
+            return gr.Row().update(visible=False), gr.Row().update(visible=True)
+
+    update_button.click(fn=update_state,inputs=[],outputs=[queue_evn_buttons,noqueue_evn_buttons])
 
     cancel_button.click(handle_cancel, inputs=[], outputs=[cancel_button])
-
     start_button.click(
         fn=process_video, 
+        inputs=[input_video_mode,upload_video,video_path,input_folder,force_fps,resize_width,resize_height,target_fps,seed,sampler,sampler_step,cfg_scale,temporalnet_ver,temporalnet_model,temporalnet_weight,prompt,neg_prompt,denoise,output_images_folder,overwrite_output_images,output_video], 
+        outputs=[status, check_complete, start_button, cancel_button]
+    )
+
+    cancel_noqueue_button.click(handle_cancel_noqueue, inputs=[], outputs=[cancel_button])
+    start_noqueue_button.click(
+        fn=process_video_noqueue, 
         inputs=[input_video_mode,upload_video,video_path,input_folder,force_fps,resize_width,resize_height,target_fps,seed,sampler,sampler_step,cfg_scale,temporalnet_ver,temporalnet_model,temporalnet_weight,prompt,neg_prompt,denoise,output_images_folder,overwrite_output_images,output_video], 
         outputs=[status, check_complete, start_button, cancel_button]
     )
